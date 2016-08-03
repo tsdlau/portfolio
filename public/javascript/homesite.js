47f1f12b08f1b309366ab787b1aff4765e0db3cc
dickson <kr.d.tsl@gmail.com>
2016-08-03 14:09:02 -0400

    added modals added waves.effects

--- public/javascript/homesite.js
+++ public/javascript/homesite.js
@@ -22,4 +22,14 @@ $(document).ready(function() {
       scrollTop: $(href).offset().top
     }, 500);
   });
+
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .6, // Opacity of modal background
+      in_duration: 400, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+    }
+  );
 });
