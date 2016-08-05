960b0c14a6948ac6e348a90f81f56eea5b6a881f
dickson <kr.d.tsl@gmail.com>
2016-08-04 20:40:39 -0400

    changed offset for scroll top

--- public/javascript/homesite.js
+++ public/javascript/homesite.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
   var offset = 300,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 5000,
+		offset_opacity = 1300,
 		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
 		//grab the "back to top" link
