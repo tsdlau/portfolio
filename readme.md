f48d8d0c7263860994ef6d6c5fb270ad5c3cf790
Dickson Lau <dickson@thelevelup.com>
2017-07-06 15:31:51 -0400

    still making scrub mistakes ayy

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-this is the source code for my personal website. 
+this is the source code for my personal website.
 
 this baby is built on sinatra slapped with some javascript with the view layers written in haml.
 
 [check it out](http://tsdlau.com)
+
+note to self: use `shotgun` to spin up locally. `ruby server.rb` will ignore `config.ru`
--- server.rb
+++ server.rb
@@ -1,34 +1,37 @@
-require "sinatra"
+require 'sinatra'
 require 'hamlit'
 
 use Rack::Deflater
 set :haml, { escape_html: false }
 
-
-get "/" do
-  redirect "/home"
+get '/' do
+  redirect '/home'
 end
 
-get "/home" do
-  haml :"home/home", layout: :"home/layout"
+get '/home' do
+  haml :'home/home', layout: :'home/layout'
 end
 
-# get "/photos" do
-#   haml :"home/photos", layout: false
+# get '/photos' do
+#   haml :'home/photos', layout: false
 # end
 
-get "/resume" do
-  send_file File.join(settings.public_folder, "/resume.pdf")
+get '/resume' do
+  send_file File.join(settings.public_folder, '/resume.pdf')
 end
 
-get "/blog" do
-  haml :"blog/blog", layout: :"blog/layout_blog"
+get '/blog' do
+  haml :'blog/blog', layout: :'blog/layout_blog'
 end
 
-get "/microinteractions" do
-  haml :"blog/microinteractions", layout: :"blog/layout_blog"
+get '/microinteractions' do
+  haml :'blog/microinteractions', layout: :'blog/layout_blog'
 end
 
-get "/parallax" do
-  haml :"blog/parallax", layout: :"blog/layout_blog"
+get '/groupdynmics' do
+  haml :'blog/groupdynamics', layout: :'blog/layout_blog'
+end
+
+get '/parallax' do
+  haml :'blog/parallax', layout: :'blog/layout_blog'
 end
